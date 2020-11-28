a58b5e4628a62938fa93c266b5462813d60be392
Glazynov A <glazunov_ay@lemz-t.ru>
2020-11-29 01:11:37 +0700

    add color scheme

---
+++ lib/misc/app_color_scheme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+
+class ColorSheme {
+  // Green colors (from dark to light).
+  static final Color primary      = Color(0xFF009A6E);
+  static final Color primaryDark  = Color(0xFF006A4A);
+  static final Color primaryLight = Color(0xFF00CE93);
+  static final Color primary12    = Color(0xFF1FA67F);
+  static final Color primary60    = Color(0xFF99D6C5);
+  static final Color primary80    = Color(0xFFCCEAE2);
+
+  // Monochrome colors (from dark to light).
+  static final Color black   = Color(0xFF000000);
+  static final Color gray85  = Color(0xFF262626);
+  static final Color gray50  = Color(0xFF797979);
+  static final Color gray40  = Color(0xFF999999);
+  static final Color gray22  = Color(0xFFC7C7C7);
+  static final Color gray12  = Color(0xFFE1E1E1);
+  static final Color bgWhite = Color(0xFFF4F4F4);
+  static final Color white   = Color(0xFFFFFFFF);
+
+  static final Color warning = Color(0xFFFFCB00);
+  static final Color accept  = Color(0xFFFFA100);
+  static final Color danger  = Color(0xFFD51C1C);
+}
